3a225b7ae6d416f600905cc92e702ae3d6025dbe
Mohammed Dharhan <m.dharhan@gmail.com>
2023-03-28 14:16:56 -0700

    variable labeling

--- Programs/ball.py
+++ Programs/ball.py
@@ -1,8 +1,8 @@
 # Program to calculate the height of a ball in veritcal motion
 
-v = 5
-g = 9.81 
-t = 0.6
+v = 5 #velocity
+g = 9.81 #acceleratin of gravity
+t = 0.6 #time
 
 y = v*t - 0.5*g*t**2
 
