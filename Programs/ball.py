78f3d7362269e1750ddf310ea41a7e3874dd91c2
Mohammed Dharhan <m.dharhan@gmail.com>
2023-03-29 14:37:55 -0700

    Added Visualization Curve

--- Programs/ball.py
+++ Programs/ball.py
@@ -1,9 +1,15 @@
 # Program to calculate the height of a ball in veritcal motion
+import numpy as np 
+import matplotlib.pyplot as plt
+
 
 v = 5 #velocity
-g = 9.81 #acceleratin of gravity
-t = 0.6 #time
+g = 9.81 #acceleration of gravity
+t = np.linspace(0, 1, 1001)
 
 y = v*t - 0.5*g*t**2
 
-print(y)
+plt.plot(t, y)
+plt.xlabel('t (s)')
+plt.ylabel('y (m)')
+plt.show()
